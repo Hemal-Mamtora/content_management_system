2783c75392844a20232142f5b773fd74a8995e8b
Hemal Mamtora <hemalmamtora1@gmail.com>
2023-03-25 17:27:50 +0530

    Update settings.py

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+*py$^e0pn&b@3vk#v3a%^w)_5!6w@+^7*t^^95w+7bk%!j0od'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
